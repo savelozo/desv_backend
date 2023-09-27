ed88ec5ccf00e97999bb3a4e877e10b899d98903
savelozo <savelozo@uc.cl>
2023-09-27 20:29:02 -0300

    Change static root

--- desv_backend/settings.py
+++ desv_backend/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = "/home/myusername/myproject/static"
+STATIC_ROOT = "/home/savelozo/desv_backend/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
