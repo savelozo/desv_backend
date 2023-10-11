a9909d16ddbe3a8f5852aa72c292556946b39624
savelozo <savelozo@uc.cl>
2023-10-11 20:27:52 -0300

    Add submit_data_view

--- desv_form/views.py
+++ desv_form/views.py
@@ -3,13 +3,18 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 
 @csrf_exempt
-def submit_data(request):
-    print("Request: {}".format(request.method))
+def submit_data_view(request):
     if request.method == 'POST':
-        data = json.loads(request.body)
-        # Aquí puedes procesar los datos y guardarlos en la base de datos
-        # por ejemplo: nombre = data['firstName'], apellido = data['lastName'], etc.
-        # Realiza las operaciones necesarias y devuelve una respuesta
-        response_data = {'message': 'Datos recibidos con éxito'}
+        body_unicode = request.body.decode('utf-8')
+        body_data = json.loads(body_unicode)    
+        # Obtén los datos del formulario
+        # Procesa y almacena los datos según tus necesidades
+        # ...
+
+        # Devuelve una respuesta al cliente
+        response_data = {'message': 'Datos recibidos exitosamente'}
         return JsonResponse(response_data)
+
+    # Maneja otros métodos HTTP si es necesario
+    return JsonResponse({'message': 'Método no permitido'}, status=405)
 
