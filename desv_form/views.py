5e37ed46bbea399c0215e10f7048f2c66cbda8f2
savelozo <savelozo@uc.cl>
2023-10-11 20:33:37 -0300

    Add feature: save evaluate_form on db

--- desv_form/views.py
+++ desv_form/views.py
@@ -1,19 +1,33 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from .models import EvaluateForm
 import json
 
 @csrf_exempt
 def submit_data_view(request):
     if request.method == 'POST':
         body_unicode = request.body.decode('utf-8')
-        body_data = json.loads(body_unicode)    
-        # Obtén los datos del formulario
-        # Procesa y almacena los datos según tus necesidades
-        # ...
+        body_data = json.loads(body_unicode) 
 
-        # Devuelve una respuesta al cliente
-        response_data = {'message': 'Datos recibidos exitosamente'}
-        return JsonResponse(response_data)
+        # Crea una instancia del modelo EvaluateForm y guarda los datos
+        evaluate_form = EvaluateForm(
+            name=body_data.get('firstName', ''),
+            last_name=body_data.get('lastName', ''),
+            email=body_data.get('email', ''),
+            date_of_birth=body_data.get('birthdate', ''),
+            nationality=body_data.get('nationality', ''),
+            address=body_data.get('address', ''),
+            education_level=body_data.get('educationLevel', ''),
+            employment_status=body_data.get('employmentStatus', ''),
+            industry_type=body_data.get('industry', ''),
+            #monthly_income=body_data.get('incomeLevel', '')
+        )
+
+        # Guarda la instancia del modelo en la base de datos
+        evaluate_form.save()
+
+        # Devuelve una respuesta exitosa
+        return JsonResponse({'message': 'Datos guardados con éxito'}, status=200)
 
     # Maneja otros métodos HTTP si es necesario
     return JsonResponse({'message': 'Método no permitido'}, status=405)
